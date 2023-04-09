70143c66d4330ba85a425016857e90ad11ae9f0f
hongui <honge7694@naver.com>
2023-04-09 09:54:20 +0900

    웹소켓을 이용한 실시간 create, update, delete 처리

--- app/consumers.py
+++ app/consumers.py
@@ -3,10 +3,32 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 
+class LiveblogConsumer(WebsocketConsumer):
+    groups = ["liveblog"] # 메세지를 받을 그룹명
+
+    '''
+    그룹을 통해 받은 메세지를 그대로 웹소켓 클라이언트에게 전달한다. (self.send(전달할_메세지))
+    메세지의 type 값과 같은 이름의 메서드가 호출된다.
+
+    ex) type "liveblog.post.created" => "liveblog_post_created" 메서드 호출 => 마침표(.)를 언더바(_)로 바꾼다.
+    '''
+
+    def liveblog_post_created(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+    def liveblog_post_updated(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+    def liveblog_post_deleted(self, event_dict):
+        self.send(json.dumps(event_dict))
+
+
 class EchoConsumer(WebsocketConsumer):
-    # 웹소켓 클라이언트에서
-    # text frame 으로 보내면 text_data 인자에 값이 담겨지고
-    # binary data frame 으로 보내면 bytes_data 인자에 값이 담겨져 호출된다.
+    '''
+    웹소켓 클라이언트에서
+    text frame 으로 보내면 text_data 인자에 값이 담겨지고
+    binary data frame 으로 보내면 bytes_data 인자에 값이 담겨져 호출된다.
+    '''
 
     def receive(self, text_data=None, bytes_data=None):
         # 새로운 text/bytes frame을 받을 때마다 호출된다.
--- app/routing.py
+++ app/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.consumers import EchoConsumer
+from app.consumers import EchoConsumer, LiveblogConsumer
 
 
 # 장고의 urls urlpatterns와 유사한 역할 (URL 매핑)
@@ -7,5 +7,6 @@ from app.consumers import EchoConsumer
 # 장고에서 찾아서 읽어가는 것이 아니라, asgi.py 직접 임포트하여 지정하기때문에
 # websocket_urlpatterns는 이름을 다르게하여도 괜찮다.
 websocket_urlpatterns = [
+    path("ws/liveblog/", LiveblogConsumer.as_asgi()),
     path("ws/echo/", EchoConsumer.as_asgi()),
 ]
