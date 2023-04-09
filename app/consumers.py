6d3467e439111c52de7f086bb6cae40806f074eb
hongui <honge7694@naver.com>
2023-04-09 10:08:25 +0900

    JsonWebsocketConsumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,9 +1,9 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
 
 
-class LiveblogConsumer(WebsocketConsumer):
+class LiveblogConsumer(JsonWebsocketConsumer):
     groups = ["liveblog"] # 메세지를 받을 그룹명
 
     '''
@@ -14,30 +14,41 @@ class LiveblogConsumer(WebsocketConsumer):
     '''
 
     def liveblog_post_created(self, event_dict):
-        self.send(json.dumps(event_dict))
+        # self.send(json.dumps(event_dict)) # WebsocketConsumer에서 사용.
+        self.send_json(event_dict)
 
     def liveblog_post_updated(self, event_dict):
-        self.send(json.dumps(event_dict))
+        # self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
     def liveblog_post_deleted(self, event_dict):
-        self.send(json.dumps(event_dict))
+        # self.send(json.dumps(event_dict))
+        self.send_json(event_dict)
 
 
-class EchoConsumer(WebsocketConsumer):
+class EchoConsumer(JsonWebsocketConsumer):
     '''
     웹소켓 클라이언트에서
     text frame 으로 보내면 text_data 인자에 값이 담겨지고
     binary data frame 으로 보내면 bytes_data 인자에 값이 담겨져 호출된다.
     '''
-
-    def receive(self, text_data=None, bytes_data=None):
+    def receive(self, content, **kwargs):
         # 새로운 text/bytes frame을 받을 때마다 호출된다.
-        obj = json.loads(text_data)
-        print("수신 : ", obj)
+        print("수신 : ", content)
 
-        json_string = json.dumps({
-            "content": obj['content'],
-            "user": obj['user'],
+        self.send_json({
+            "content": content["content"],
+            "user": content["user"],
         })
 
-        self.send(json_string)
+    # def receive(self, text_data=None, bytes_data=None):
+    #     # 새로운 text/bytes frame을 받을 때마다 호출된다.
+    #     obj = json.loads(text_data)
+    #     print("수신 : ", obj)
+
+    #     json_string = json.dumps({
+    #         "content": obj['content'],
+    #         "user": obj['user'],
+    #     })
+
+    #     self.send(json_string)
