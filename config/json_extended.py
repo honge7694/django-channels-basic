49e0447497a37b1a4e87dbf7c2600d3e90224e37
hongui <honge7694@naver.com>
2023-04-17 21:01:52 +0900

    채팅방 참여자 목록 - RoomMember 모델 생성 및 커스텀 json encoder/decoder 구현

---
+++ config/json_extended.py
@@ -0,0 +1,22 @@
+import json
+
+
+class ExtendedJSONEncoder(json.JSONEncoder):
+    # 인코딩시 집합자료 구조일 경우, 사전타입으로 변환.
+    def default(self, obj):
+        if isinstance(obj, set):
+            return {"__set__": True, "values": tuple(obj)}
+        return obj
+
+
+class ExtendedJSONDecoder(json.JSONDecoder):
+    # 커스텀한 집합자료구조일 경우, 집합으로 변환.
+    def __init__(self, **kwargs):
+        kwargs.setdefault("object_hook", self._object_hook)
+        super().__init__(**kwargs)
+    
+    @staticmethod
+    def _object_hook(dct):
+        if '__set__' in dct:
+            return set(dct('values'))
+        return dct
